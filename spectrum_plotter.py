63fd78d4e98f73d09ecff07307d372545b2085b3
George Duffett <george@nas-10-241-70-146.york.ac.uk>
2019-11-22 14:47:12 +0000

    Added Fano function and Least Squares fano function

--- spectrum_plotter.py
+++ spectrum_plotter.py
@@ -5,8 +5,35 @@ import numpy as np
 ## We fit a "Fano lineshape" to the peak in the spectrum
 ## Python needs a Fano function, defined at the top to
 ## fit this curve
-def fano:
-    pass
+def fano(x, x0, b, c, A, e):
+    """The Fano lineshape for reflection.
+
+    x: x coordinates
+    x0: Peak position
+    b: Peak width
+    c: Asymmetry parameter
+    A: Amplitude
+    e: Offset
+
+    y: The y coordinates of the Fano lineshape"""
+    eps1 = 2*(x-x0)/b1
+    f1 = (eps+c)**2/(eps**2+1)
+    y = A*f+e
+    return y
+
+def f_lsq(params, x, y_meas):
+    """ Computes the deviation of spetrum data
+    from a Fano lineshape.
+
+    params: List of Fano parameters x0, b, c, A, e
+    x: the x coordinates of the measurement
+    y: the measured y data
+
+    res: the residules
+    """
+    y_fan = fano(x, *params)
+    res = y_fan-y_meas
+    return res
 
 ## Set wavelength range)
 xi = 700
